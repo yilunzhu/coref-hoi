b2eb3376dda0b20b7a42698d3ac0e2d8f3f2c03e
Yilun Zhu <allen.yl.zhu@gmail.com>
2022-10-11 14:33:07 -0400

    fix bugs

--- model.py
+++ model.py
@@ -163,7 +163,7 @@ class CorefModel(nn.Module):
             same_sg_span = (same_sg_start & same_sg_end).to(torch.long)
             sg_labels = torch.matmul(torch.unsqueeze(gold_sg_cluster_map, 0).to(torch.float), same_sg_span.to(torch.float))
             sg_labels = torch.squeeze(sg_labels.to(torch.long), 0)
-        elif conf['hard_encode']:
+        elif conf['sg_type'] == 'hard_encode':
             same_sg_start = (torch.unsqueeze(gold_sg_starts, 1) == torch.unsqueeze(candidate_starts, 0))
             same_sg_end = (torch.unsqueeze(gold_sg_ends, 1) == torch.unsqueeze(candidate_ends, 0))
             same_sg_span = (same_sg_start & same_sg_end).to(torch.long)
