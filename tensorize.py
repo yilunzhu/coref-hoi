c22e75472cf06393c63985909ff82309cbc59afe
Yilun Zhu <allen.yl.zhu@gmail.com>
2022-10-29 23:27:40 -0400

    debug

--- tensorize.py
+++ tensorize.py
@@ -75,14 +75,14 @@ class CorefDataProcessor:
                 'trn': join(self.data_dir, self.config['dataset'], f'{to_add}.{self.language}.{self.max_seg_len}.jsonlines'),
                 # 'dev': join(self.data_dir, self.config['dataset'], f'dev.{self.language}.{self.max_seg_len}.jsonlines'),
                 # 'tst': join(self.data_dir, self.config['dataset'], f'test.{self.language}.{self.max_seg_len}.jsonlines')
-                'dev': join(self.data_dir, 'ontogum', f'test.gum.{self.language}.{self.max_seg_len}.jsonlines'),
+                'dev': join(self.data_dir, 'ontogum', f'dev.gum.{self.language}.{self.max_seg_len}.jsonlines'),
                 'tst': join(self.data_dir, 'ontogum', f'test.gum.{self.language}.{self.max_seg_len}.jsonlines')
             }
             singleton_paths = {
                 'trn': join(self.data_dir, self.config['dataset']+'_sg', f'{to_add}.{self.language}.{self.max_seg_len}.jsonlines'),
                 # 'dev': join(self.data_dir, self.config['dataset']+'_'+self.config["singleton_suffix"], f'dev_{self.config["singleton_suffix"]}.{self.language}.{self.max_seg_len}.jsonlines'),
                 # 'tst': join(self.data_dir, self.config['dataset']+'_'+self.config["singleton_suffix"], f'test_{self.config["singleton_suffix"]}.{self.language}.{self.max_seg_len}.jsonlines')
-                'dev': join(self.data_dir, 'ontogum_'+self.config["singleton_suffix"], f'test.gum.{self.language}.{self.max_seg_len}.jsonlines'),
+                'dev': join(self.data_dir, 'ontogum_'+self.config["singleton_suffix"], f'dev.gum.{self.language}.{self.max_seg_len}.jsonlines'),
                 'tst': join(self.data_dir, 'ontogum_'+self.config["singleton_suffix"], f'test.gum.{self.language}.{self.max_seg_len}.jsonlines')
             }
             for split, path in paths.items():
